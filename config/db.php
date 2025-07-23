1797e25f5ef116d2645cf32a7195bec4022d542c
hafsaArifDev <hafsaarifbintearif@gmail.com>
2025-07-23 23:26:07 +0500

    database connection

--- config/db.php
+++ config/db.php
@@ -1,13 +1,10 @@
 <?php
 
 $host = "localhost";
-$dbname = "hospital system";
+$dbname = "hospital_system";
 $username = "root";
 $password = "";
 
-$connection = new PDO("mysql:host=localhost;dbname=hospital system", "root", "");
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = new PDO("mysql:host=localhost;dbname=hospital_system", "root", "");
+
 ?>
