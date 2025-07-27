a49ebc603d390b0040fc64edd973e1735f356fef
hafsaArifDev <hafsaarifbintearif@gmail.com>
2025-07-27 23:22:23 +0500

    css

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
 <?php include 'includes/header.php'; ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index.php</title>
+    <style>
+        .main-section{
+    background-color: rgba(0,0,0,0.1);
+    height: 200px;
+    width: 100%;
+    margin: 0px;
+    padding: 0px;
+}
+h2 {
+    padding: 20px , 50px;
+    text-align: center;
+    color: #3498db;
+    font-size: 43px;
+}
+.img{
+    background: url("images/image1.jpeg");
+}
+p{
+     text-align: center;
+    
+}
+    </style>
+</head>
+<body class="main-section">
+    <div >
 <h2>Welcome to Hospital Management System</h2>
+<div class="img"></div>
 <p>This system allows patients to book appointments, doctors to manage prescriptions, and admins to control all activities.</p>
+</div>
+</body>
+</html>
 
 <?php include 'includes/footer.php'; ?>
